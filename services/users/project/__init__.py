b75577f9d00635410386ca956fbdd4e1df593ffe
Jeffrey Monsalve <jeffrey.monsalve@Jeffreys-MBP.fios-router.home>
2020-03-24 22:33:43 -0400

    docker_config

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,8 +1,9 @@
 # services/users/project/__init__.py
 
-
+import os # new 
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+import sys # new
 
 
 # instantiate the app
@@ -11,7 +12,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig') # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
 
 
 class UsersPing(Resource):

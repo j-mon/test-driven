48a96d9eb7190c448c834d1979d76d9ba333093a
Jeffrey Monsalve <jeffrey.monsalve@Jeffreys-MBP.fios-router.home>
2020-03-27 10:01:49 -0400

    Part 1 Finished

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,27 +1,35 @@
 # services/users/project/__init__.py
 
-import os # new 
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-import sys # new
+
+import os
+
+from flask import Flask # new
+from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
+# instantiate the db
+db = SQLAlchemy()
 
-api = Api(app)
+# new 
+def create_app(script_info=None):
 
-# set config
-app_settings = os.getenv('APP_SETTINGS')  # new
-app.config.from_object(app_settings)      # new
+    # instantiate the app
+    app = Flask(__name__)
 
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+    # set up extensions
+    db.init_app(app)
 
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-api.add_resource(UsersPing, '/users/ping')
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db':db}
+
+    return app
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,21 +1,29 @@
 # services/users/project/config.py
 
 
+import os # new 
+
+
 class BaseConfig:
     """Base configuration"""
     Testing = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # new
+    SECRET_KEY = 'my_precious' # new 
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # new
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL') # new
 
 
 class ProductionConfig(BaseConfig):
     """Production configurstion"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # new
+
+
